a9b542bd199494b99e91977abbea6d6244b14ed1
AmberComet <supercouch39@cox.net>
2023-02-14 23:46:34 -0600

    Started File IO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{fs::File, io};
+
 fn main() {
-    println!("Hello, world!");
+    
+}
+
+fn words_initalization() -> Vec<String> {
+    let file = File::open(Collins_Scrabble_Words_2019.txt).expect("Error: Cant Find File");
+    let buf = io::BufReader::new(file);
+    io::BufRead::lines(buf)
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
 }
